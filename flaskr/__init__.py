21995c2080b23be8233c31713648cd78c768c756
Wang Ao <1366463855@qq.com>
2019-07-05 16:37:35 +0800

    Init Flask App

---
+++ flaskr/__init__.py
@@ -0,0 +1,25 @@
+from flask import Flask
+import os
+from . import db
+
+def create_app(test_config=None):
+    app=Flask(__name__,instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path,'flaskr.sqlite'),
+    )
+    if test_config==None:
+        app.config.from_pyfile('config.py',silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+    db.init_app(app)
+    @app.route('/hello')
+    def hello():
+        return "Hello World"
+    
+    return app
